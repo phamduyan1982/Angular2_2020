8d18db09167b102217c54b8dccd3a14ae8e505ba
an <phamduyan1982@gmail.com>
2020-04-20 16:54:23 +0700

    session16customServicesTheEnd

--- AngularDemo/session14/app.js
+++ AngularDemo/session14/app.js
@@ -1,11 +1,15 @@
 ﻿var app = angular.module('app', []);
 app.controller('consumWsCtrl', consumWsCtrl);
 
-consumWsCtrl.$inject = ['$scope','$http'];
+consumWsCtrl.$inject = ['$scope', '$http', '$log'];
 
-function consumWsCtrl($scope, $http) {
-    $http.get('/session14/Employees.asmx/GetEmployees').then(function (response) {
-        $scope.employees = response.data;
-    });
-
+function consumWsCtrl($scope, $http, $log) {
+    $http.get('/session14/Employees.asmx/GetEmployees')
+        .then(
+            function (response) {
+                $scope.employees = response.data;
+            },
+            function (response) {
+                $log.error(response.data);
+            });
 }
---
+++ AngularDemo/session16/app.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('app', []);
+
+app.controller('processingStringCtrl', processingStringCtrl);
+
+processingStringCtrl.$inject = ['$scope','stringService'];
+
+function processingStringCtrl($scope, stringService) {
+ 
+    $scope.transformString = function (input) {
+        $scope.output = stringService.transformString(input);
+    }
+
+    $scope.toLower = function (input) {
+        $scope.output = stringService.toLower(input);
+    }
+}
